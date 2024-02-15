5c304ffeb37244f3a9d7245d846dc2bf19aa8867
[Github <dudwnsv123@naver.com>
2024-02-15 12:48:12 +0900

    List

---
+++ src/main/java/ch07_dao/Ex01_Search.java
@@ -0,0 +1,29 @@
+package ch07_dao;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/ch07/city/search")
+public class Ex01_Search extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int id = (request.getParameter("id") == null) ? 2340 : Integer.parseInt(request.getParameter("id"));
+		
+		CityDao cDao = new CityDao();
+		City city = cDao.getCity(id);
+		System.out.println(city);
+		
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.print(city);
+	}
+
+	
+
+}
---
+++ src/main/webapp/ch07/list.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>City List</title>
+<style>
+	th, td { padding: 3px; }
+</style>
+</head>
+<body style="margin: 50px">
+	<h1>City List</h1>
+	<hr>
+	<table border= "1">
+		<tr><th>아이디</th><tr><th>도시명</th><tr><th>국가코드</th><tr><th>지역명</th><tr><th>인구수</th>
+		
+	</table>
+</body>
+</html>
