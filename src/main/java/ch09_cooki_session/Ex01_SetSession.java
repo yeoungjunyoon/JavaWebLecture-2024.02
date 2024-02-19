31eb788b33f1a0b794abbee539cf48d568fbd286
[Github <dudwnsv123@naver.com>
2024-02-19 12:46:01 +0900

    Cookie

---
+++ src/main/java/ch09_cooki_session/Ex01_SetSession.java
@@ -0,0 +1,38 @@
+package ch09_cooki_session;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/ch09/setSession")
+public class Ex01_SetSession extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		
+//		PrintWriter out = response.getWriter();
+//		response.setContentType("text/html; charset=utf-8");
+//		out.print("<h1>isNew: " + session.isNew());
+		
+		session.setAttribute("price", 12500);
+		session.setAttribute("uid", "james");
+		String[] fruits = {"apple", "banana", "cherry"};
+		session.setAttribute("fruits", fruits);
+		
+		session.setMaxInactiveInterval(24 * 60 * 60); 		// 24시간
+		
+//		session.invalidate();
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/ch09/session.jsp");
+		rd.forward(request, response);
+	}
+
+}
---
+++ src/main/java/ch09_cooki_session/Ex03_SetCooki.java
@@ -0,0 +1,36 @@
+package ch09_cooki_session;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URLEncoder;
+import java.util.Date;
+
+@WebServlet("/ch09/setCookie")
+public class Ex03_SetCooki extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html; charset=utf-8");
+		
+		Cookie c1 = new Cookie("cookie-name", "cookie-value");
+		c1.setMaxAge(24 * 60 * 60); 		// 유효기간: 24 * 60 * 60 초 --> 1일
+		response.addCookie(c1);
+		
+		String KMsg = URLEncoder.encode("한글 데이터", "utf-8");
+		Cookie c2 = new Cookie("hangul-name", KMsg);
+		c2.setMaxAge(-1);					// 브라우저가 닫히면 사라짐
+		response.addCookie(c2);
+		
+		out.write("<h1>현재시간: " + new Date() + "</h1>");
+	}
+
+
+}
