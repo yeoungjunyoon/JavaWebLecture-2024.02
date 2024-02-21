d9382416ae020773c75c206c6c21fd331ef0aa0f
[Github <dudwnsv123@naver.com>
2024-02-21 12:46:02 +0900

    JSTL

--- src/main/java/ch10_filter/EncodingFilter.java
+++ src/main/java/ch10_filter/EncodingFilter.java
@@ -15,7 +15,7 @@ public class EncodingFilter extends HttpFilter implements Filter {
        
     public EncodingFilter() {
         super();
-      System.out.println("생성자");
+ //    System.out.println("생성자");
     }
 
 	public void destroy() {
@@ -25,12 +25,12 @@ public class EncodingFilter extends HttpFilter implements Filter {
 	// 필요한 작업을 코딩하는 부분
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		request.setCharacterEncoding("utf-8");
-		System.out.println("dofilter()");
+//		System.out.println("dofilter()");
 		chain.doFilter(request, response);
 	}
 	
 	public void init(FilterConfig fConfig) throws ServletException {
-		System.out.println("init()");
+//		System.out.println("init()");
 	}
 
 }
