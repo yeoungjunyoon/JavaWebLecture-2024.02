0907014b7d3dc63459c2df8e28f0d359134b843b
[Github <dudwnsv123@naver.com>
2024-02-22 10:54:01 +0900

    BBS Project

---
+++ src/main/java/project/controller/Exercise.java
@@ -0,0 +1,10 @@
+package project.controller;
+
+public class Exercise {
+
+	public static void main(String[] args) {
+		System.out.println((int) Math.ceil(0.1));
+		System.out.println((int) Math.ceil(1.0));
+	}
+
+}
