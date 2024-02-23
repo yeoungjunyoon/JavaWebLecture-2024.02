79d593fa3888088836f72da7274c66dd940fd5c0
[Github <dudwnsv123@naver.com>
2024-02-23 16:16:31 +0900

    BBS PROJECT

--- src/main/java/project/service/BoardServiceImpl.java
+++ src/main/java/project/service/BoardServiceImpl.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package project.service;
 
 import java.util.List;
@@ -35,7 +34,6 @@ public class BoardServiceImpl implements BoardService {
 	@Override
 	public void updateBoard(Board board) {
 		bDao.updateBoard(board);
-		
 	}
 
 	@Override
@@ -53,60 +51,4 @@ public class BoardServiceImpl implements BoardService {
 		bDao.increaseCount("reply", bid);
 	}
 
-=======
-package project.service;
-
-import java.util.List;
-
-import project.dao.BoardDao;
-import project.entity.Board;
-
-public class BoardServiceImpl implements BoardService {
-	private BoardDao bDao = new BoardDao();
-
-	@Override
-	public List<Board> getBoardList(int page, String field, String query) {
-		int offset = (page - 1) * COUNT_PER_PAGE;
-		query = "%" + query + "%";
-		List<Board> list = bDao.getBoardList(field, query, COUNT_PER_PAGE, offset);
-		return list;
-	}
-
-	@Override
-	public Board getBoard(int bid) {
-		return bDao.getBoard(bid);
-	}
-
-	@Override
-	public int getBoardCount(String field, String query) {
-		return bDao.getBoardCount(field, query);
-	}
-
-	@Override
-	public void insertBoard(Board board) {
-		bDao.insertBoard(board);
-	}
-
-	@Override
-	public void updateBoard(Board board) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void deleteBoard(int bid) {
-		bDao.deleteBoard(bid);
-	}
-
-	@Override
-	public void increaseViewCount(int bid) {
-		bDao.increaseCount("view", bid);
-	}
-
-	@Override
-	public void increaseReplyCount(int bid) {
-		bDao.increaseCount("reply", bid);
-	}
-
->>>>>>> f20f4f64a55b5020e2660caf148f7769a2ac4eef
 }
